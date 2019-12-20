e67356eaabfb3148c5239efe87338003f11c3759
julemai <juliane.mai@uwaterloo.ca>
2019-12-19 22:07:20 -0500

    adjust submit script to run all basins with 1000 sets

--- scripts/submit-array-to-graham.sh
+++ scripts/submit-array-to-graham.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-#SBATCH --mem-per-cpu=3G
+# submit with:
+#       sbatch submit-to-graham.sh   
+
+#SBATCH --mem-per-cpu=50G                          # memory; default unit is megabytes
 #SBATCH --output=/dev/null
-#SBATCH --time=1:30:00
+#SBATCH --time=6-18:00:00
 #SBATCH --account=rpp-hwheater 
 #SBATCH --mail-user=juliane.mai@uwaterloo.ca
 #SBATCH --mail-type=FAIL
-#SBATCH --job-name=test-array
-#SBATCH --array=1-100
+#SBATCH --job-name=sa-canopex
+#SBATCH --array=1-645
 
 # job-id  :: ${SLURM_ARRAY_JOB_ID}
 # task-id :: ${SLURM_ARRAY_TASK_ID}
 
 ntasks=100
-nsets=2
+nsets=1000
 
-# nsets =  10 -->  7.0min/basin  --> 6.8h for each of 100 tasks 
-# nsets =   2 -->  1.6min/basin  --> 1h 50min for each of 100 tasks 
+# nsets =   10 -->      7.0min/basin  --> 6.8h for each of 100 tasks                 #SBATCH --mem-per-cpu=3G
+# nsets =    2 -->      1.6min/basin  --> 1h 50min for each of 100 tasks             #SBATCH --mem-per-cpu=3G
+# nsets = 1000 -->    18:00:00/basin  --> 6-18:00:00 for 645 tasks (9 basins each)   #SBATCH --mem-per-cpu=50G
 
 # change to right dir
 cd /home/julemai/projects/rpp-hwheater/julemai/sa-usgs-canopex/scripts
@@ -27,7 +31,7 @@ source ../env-3.5/bin/activate
 folders=$(  \ls -d ../data_in/data_obs/*/ | cut -d '/' -f 4 | sort )
 
 # select only folders that don't have yet a results file
-# >>>>> if run all basins again then uncomment this line
+# >>>>> if run all basins again then uncomment following line
 folders=$( for ff in ${folders} ; do if [ ! -e ../data_out/${ff}/results_nsets${nsets}.pkl ] ; then echo ${ff} ; fi ; done )
 
 nfolders=$( for ff in ${folders} ; do echo ${ff} ; done | wc -l )         # 5797 (or whatever is not done yet)
@@ -56,7 +60,31 @@ if [[ ${step} -gt 0 ]] ; then
     basins_to_process=$( for ff in ${folders} ; do echo ${ff} ; done | sort | head -${end} | tail -${step} )
     
     # echo ${basins_to_process}
-    python raven_sa-usgs-canopex.py -i "${basins_to_process}" -n ${nsets}
+
+    # # ---------------------
+    # # this can be used to run multiple basins in one shot
+    # python raven_sa-usgs-canopex.py -i "${basins_to_process}" -n ${nsets}
+    # # ---------------------
+
+    # ---------------------
+    # this is running one basin after the other
+    for bb in ${basins_to_process} ; do
+
+	# actual analysis
+	python raven_sa-usgs-canopex.py -i ${bb} -n ${nsets}
+
+	# extract only sensitivity results from pickle file
+	pickle_all="../data_out/${bb}/results_nsets${nsets}.pkl"
+	pickle_si="../data_out/${bb}/sensitivity_nsets${nsets}.pkl"
+	python extract_si_results_from_pkl.py -i ${pickle_all} -o ${pickle_si}
+    
+	# plot results
+	python figure_2.py -t pdf -p ../data_out/${bb}/${bb} -i ../data_out/${bb}/results_nsets${nsets}.pkl
+	pdfcrop ../data_out/${bb}/${bb}.pdf
+	mv ../data_out/${bb}/${bb}-crop.pdf ../data_out/${bb}/${bb}.pdf
+
+    done
+    # ---------------------
     
 else
     echo 'Nothing to do!'
