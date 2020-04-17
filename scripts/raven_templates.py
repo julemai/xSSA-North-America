d60c502e301237512e2604cac7006f3c915e0c85
julemai <juliane.mai@uwaterloo.ca>
2020-04-17 11:40:39 -0400

    adjusting templates to account for new parameters

--- scripts/raven_templates.py
+++ scripts/raven_templates.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2019 Juliane Mai - juliane.mai(at)uwaterloo.ca
+# Copyright 2019-2020 Juliane Mai - juliane.mai(at)uwaterloo.ca
 #
 # License
 # This file is part of Juliane Mai's personal code library.
@@ -31,11 +31,11 @@ Written,  JM, Nov 2019
 
 RVI = """
 #########################################################################                                  
-:FileType          rvi ASCII Raven rev223 (v2.9)                                                                             
+:FileType          rvi ASCII Raven rev252 (v2.9)                                                                             
 :WrittenBy         James Craig & Juliane Mai                                                                       
-:CreationDate      November 2019
+:CreationDate      June 2019
 #
-# RAVEN run of WSC/USGS {props[id]} ({props[name]}) using weighted model options                                                                 
+# RAVEN run of WSC/USGS {props[id]} ({props[name]}) using weighted model options                                                         
 #------------------------------------------------------------------------
 #
 :StartDate               1989-01-01 00:00:00 # 1954-01-01 00:00:00 
@@ -45,13 +45,13 @@ RVI = """
 :TimeStep                1.0                                                                                   
 :Method                  ORDERED_SERIES 
 
-:PotentialMeltMethod     POTMELT_HMETS 
-:RainSnowFraction        RAINSNOW_DINGMAN    # RAINSNOW_DATA
-:SWRadiationMethod       SW_RAD_NONE         # no radiation is faster
+:PotentialMeltMethod     POTMELT_HMETS
+:RainSnowFraction        RAINSNOW_HBV        # RAINSNOW_DATA
+#:SWRadiationMethod      SW_RAD_NONE         # no radiation is faster
 :Evaporation             PET_OUDIN           # PET_DATA
 :CatchmentRoute          ROUTE_DUMP
 :Routing                 ROUTE_NONE 
-:SoilModel               SOIL_TWO_LAYER
+:SoilModel               SOIL_MULTILAYER 3
 
 :Alias DELAYED_RUNOFF CONVOLUTION[1] 
 
@@ -61,31 +61,32 @@ RVI = """
                 :Infiltration    INF_HMETS               PONDED_WATER   MULTIPLE 
                 :Infiltration    INF_VIC_ARNO            PONDED_WATER   MULTIPLE 
                 :Infiltration    INF_HBV                 PONDED_WATER   MULTIPLE 
-  :EndProcessGroup {weights[process1]}
+  :EndProcessGroup {weights[process01]}
                   :Overflow      OVERFLOW_RAVEN          SOIL[0]        DELAYED_RUNOFF
   :ProcessGroup #quickflow group
                 :Baseflow        BASE_LINEAR_ANALYTIC    SOIL[0]        SURFACE_WATER   # interflow, really
                 :Baseflow        BASE_VIC                SOIL[0]        SURFACE_WATER
                 :Baseflow        BASE_TOPMODEL           SOIL[0]        SURFACE_WATER
-  :EndProcessGroup {weights[process2]}
+  :EndProcessGroup {weights[process02]}
   :Percolation                   PERC_LINEAR             SOIL[0]        SOIL[1]         # recharge
     :Overflow                    OVERFLOW_RAVEN          SOIL[1]        DELAYED_RUNOFF
+  :Percolation                   PERC_LINEAR             SOIL[1]        SOIL[2]         # loss to deep groundwater (simplifies to HMETS when PERC_COEFF DEEP_GW=0)
   :ProcessGroup #evaporation group
                 :SoilEvaporation SOILEVAP_ALL            SOIL[0]        ATMOSPHERE      # AET
                 :SoilEvaporation SOILEVAP_TOPMODEL       SOIL[0]        ATMOSPHERE      # AET
-  :EndProcessGroup {weights[process3]}
+  :EndProcessGroup {weights[process03]}
   :Convolve                      CONVOL_GAMMA            CONVOLUTION[0] SURFACE_WATER   # 'surface runoff'
   :Convolve                      CONVOL_GAMMA_2          DELAYED_RUNOFF SURFACE_WATER   # 'delayed runoff'
   :ProcessGroup #quickflow group
                 :Baseflow        BASE_LINEAR_ANALYTIC    SOIL[1]        SURFACE_WATER
                 :Baseflow        BASE_POWER_LAW          SOIL[1]        SURFACE_WATER
-  :EndProcessGroup {weights[process4]} 
+  :EndProcessGroup {weights[process04]} 
   :ProcessGroup #snow balance group
                 :SnowBalance     SNOBAL_HMETS            MULTIPLE       MULTIPLE
                 :SnowBalance     SNOBAL_SIMPLE_MELT      SNOW           PONDED_WATER
                 :SnowBalance     SNOBAL_HBV              MULTIPLE       MULTIPLE
-                #:SnowBalance     SNOBAL_GAWSER           MULTIPLE       MULTIPLE
-  :EndProcessGroup {weights[process5]} 
+                #:SnowBalance    SNOBAL_GAWSER           MULTIPLE       MULTIPLE
+  :EndProcessGroup {weights[process05]} 
 :EndHydrologicProcesses
 
 #:CreateRVPTemplate
@@ -97,15 +98,15 @@ RVI = """
 # :WriteNetcdfFormat
 
 # Accumulated Infiltration volume
-#:CustomOutput DAILY AVERAGE Between:PONDED_WATER.And.SOIL[0] BY_BASIN
+# :CustomOutput DAILY AVERAGE Between:PONDED_WATER.And.SOIL[0] BY_BASIN
 
 # Actual snow on ground over whole basin 
-#:CustomOutput DAILY AVERAGE SNOW BY_BASIN
+# :CustomOutput DAILY AVERAGE SNOW BY_BASIN
 
 # Accumulated Snow melt volume
-#:CustomOutput DAILY AVERAGE From:SNOW BY_BASIN
+# :CustomOutput DAILY AVERAGE From:SNOW BY_BASIN
 
-:EvaluationMetrics NASH_SUTCLIFFE RMSE
+:EvaluationMetrics NASH_SUTCLIFFE RMSE KLING_GUPTA
 :SilentMode
 :DontWriteWatershedStorage
 #
@@ -114,11 +115,11 @@ RVI = """
 
 RVP = """
 #########################################################################                                  
-:FileType          rvp ASCII Raven rev223 (v2.9)                                                                             
+:FileType          rvp ASCII Raven rev252 (v2.9)                                                                             
 :WrittenBy         James Craig & Juliane Mai                                                                       
-:CreationDate      November 2019
+:CreationDate      June 2019
 #
-# RAVEN run of WSC/USGS {props[id]} ({props[name]}) using weighted model options                                                                     
+# RAVEN run of WSC/USGS {props[id]} ({props[name]}) using weighted model options                                                              
 #------------------------------------------------------------------------                                 
 #
 
@@ -138,16 +139,17 @@ RVP = """
   :Attributes,
   :Units,
   TOPSOIL,
-  PHREATIC,    
+  PHREATIC,  
+  DEEP_GW  
 :EndSoilClasses
 
 #-----------------------------------------------------------------
 # Land Use Classes
 #-----------------------------------------------------------------
 :LandUseClasses, 
-  :Attributes,        IMPERM,           FOREST_COV, 
-       :Units,          frac,                 frac, 
-       FOREST,           0.0, {props[forest_frac]},    
+  :Attributes,        IMPERM,    FOREST_COV, 
+       :Units,          frac,          frac, 
+       FOREST,           0.0,   {props[forest_frac]},    
 :EndLandUseClasses
 
 #-----------------------------------------------------------------
@@ -165,8 +167,8 @@ RVP = """
 :SoilProfiles
          LAKE, 0
          ROCK, 0
-  DEFAULT_P, 2, TOPSOIL, {par[x29]}, PHREATIC, {par[x30]}, 
-# DEFAULT_P, 2, TOPSOIL,      x(29), PHREATIC,      x(30), 
+  DEFAULT_P, 3, TOPSOIL, {par[x29]}, PHREATIC, {par[x30]},  DEEP_GW, 1e6
+# DEFAULT_P, 3, TOPSOIL,      x(29), PHREATIC,      x(30),  DEEP_GW, 1e6
 :EndSoilProfiles
 
 #-----------------------------------------------------------------
@@ -186,7 +188,8 @@ RVP = """
 :GlobalParameter         SNOW_SWI_MAX {dpar[sum_x13_x14]}   # x(13)+x(14) 
 :GlobalParameter     SWI_REDUCT_COEFF {par[x15]}            # x(15)
 :GlobalParameter             SNOW_SWI {par[x19]}            # x(19)
-#:GlobalParameter      TOC_MULTIPLIER 1.0                   # 
+:GlobalParameter        RAINSNOW_TEMP {par[x31]}            # x(31)
+:GlobalParameter       RAINSNOW_DELTA {par[x32]}            # x(32) 
 
 #-----------------------------------------------------------------
 # Soil Parameters
@@ -195,9 +198,11 @@ RVP = """
   :Parameters,        POROSITY,      PERC_COEFF,  PET_CORRECTION,  BASEFLOW_COEFF,      B_EXP,   HBV_BETA, MAX_BASEFLOW_RATE, BASEFLOW_N,      FIELD_CAPACITY,   SAT_WILT,
        :Units,               -,             1/d,               -,             1/d             
       TOPSOIL,             1.0,      {par[x28]},      {par[x08]}, {dpar[pow_x04]}, {par[x02]}, {par[x03]},        {par[x05]}, {par[x06]}, {dpar[sum_x09_x10]}, {par[x09]},
-     PHREATIC,             1.0,             0.0,             0.0, {dpar[pow_x11]},        0.0,        0.0,               0.0, {par[x12]},                 0.0,        0.0,   
+     PHREATIC,             1.0,      {par[x35]},             0.0, {dpar[pow_x11]},        0.0,        0.0,               0.0, {par[x12]},                 0.0,        0.0, 
+      DEEP_GW,             1.0,             0.0,             0.0,             0.0,        0.0,        0.0,               0.0,        0.0,                 0.0,        0.0,  
  #    TOPSOIL,             1.0,           x(28),           x(08),           x(04),      x(02),      x(03),             x(05),      x(06),         x(09)+x(10),      x(09),
  #   PHREATIC,             1.0,             0.0,             0.0,           x(11),        0.0,        0.0,               0.0,      x(12),                 0.0,        0.0,
+ #    DEEP_GW,             1.0,             0.0,             0.0,             0.0,        0.0,        0.0,               0.0,        0.0,                 0.0,        0.0,
 :EndSoilParameterList
 
 #-----------------------------------------------------------------
@@ -236,11 +241,11 @@ RVP = """
 
 RVC = """
 #########################################################################                                  
-:FileType          rvc ASCII Raven rev223 (v2.9)                                                                             
+:FileType          rvc ASCII Raven rev252 (v2.9)                                                                             
 :WrittenBy         James Craig & Juliane Mai                                                                       
-:CreationDate      November 2019
+:CreationDate      June 2019
 #
-# RAVEN run of WSC/USGS {props[id]} ({props[name]}) using weighted model options                                                                    
+# RAVEN run of WSC/USGS {props[id]} ({props[name]}) using weighted model options                                                           
 #------------------------------------------------------------------------                                 
 #
 
@@ -264,18 +269,21 @@ RVC = """
 
 RVT = """
 #########################################################################                                  
-:FileType          rvt ASCII Raven rev223 (v2.9)                                                                             
+:FileType          rvt ASCII Raven rev252 (v2.9)                                                                             
 :WrittenBy         James Craig & Juliane Mai                                                                       
-:CreationDate      November 2019
+:CreationDate      June 2019
 #
-# RAVEN run of WSC/USGS {props[id]} ({props[name]}) using weighted model options                                                                    
+# RAVEN run of WSC/USGS {props[id]} ({props[name]}) using weighted model options                                                             
 #------------------------------------------------------------------------
 
 # meteorological forcings
 :Gauge
-  :Latitude   {props[lat_deg]}
-  :Longitude  {props[lon_deg]}
-  :Elevation  {props[elevation_m]}
+  :Latitude        {props[lat_deg]}
+  :Longitude       {props[lon_deg]}
+  :Elevation       {props[elevation_m]}
+  :RainCorrection  {par[x33]}
+  :SnowCorrection  {par[x34]}
+  #
   #:RedirectToFile {props[id]}/model_basin_mean_forcing_daymet.rvt 
   :RedirectToFile {props[id]}/model_basin_mean_forcing_Santa-Clara.rvt
 :EndGauge
@@ -287,25 +295,24 @@ RVT = """
 
 RVH = """
 #########################################################################                                  
-:FileType          rvh ASCII Raven rev223 (v2.9)                                                                             
+:FileType          rvh ASCII Raven rev252 (v2.9)                                                                             
 :WrittenBy         James Craig & Juliane Mai                                                                       
-:CreationDate      November 2019
+:CreationDate      June 2019
 #
-# RAVEN run of WSC/USGS {props[id]} ({props[name]}) using weighted model options                                                            
+# RAVEN run of WSC/USGS {props[id]} ({props[name]}) using weighted model options                                                         
 #------------------------------------------------------------------------                            
 #                                                                                                           
 #                                                                                                               
 :SubBasins                                                                                                              
-        :Attributes     NAME         DOWNSTREAM_ID   PROFILE   REACH_LENGTH    GAUGED                                                          
-        :Units          none         none            none      km              none                                                                                                    
-        1,             {props[id]},  -1,             NONE,     _AUTO,          1
+        :Attributes     NAME    DOWNSTREAM_ID   PROFILE   REACH_LENGTH    GAUGED                                                          
+        :Units          none    none            none      km              none                                                                                                    
+        1,       {props[id]},   -1,             NONE,     _AUTO,          1
 :EndSubBasins                                                                                                                           
                                                                                                                 
 :HRUs                                                                                                           
-        :Attributes   AREA               ELEVATION             LATITUDE          LONGITUDE         BASIN_ID  LAND_USE_CLASS  VEG_CLASS SOIL_PROFILE AQUIFER_PROFILE TERRAIN_CLASS    SLOPE   ASPECT  
-        :Units        km2                m                     deg               deg               none      none            none      none         none            none             deg      deg     
-                 1    {props[area_km2]}, {props[elevation_m]}, {props[lat_deg]}, {props[lon_deg]}, 1         FOREST          FOREST    DEFAULT_P    [NONE]          DEFAULT_T        {props[slope_deg]}        0
+        :Attributes   AREA              ELEVATION            LATITUDE         LONGITUDE  BASIN_ID  LAND_USE_CLASS  VEG_CLASS SOIL_PROFILE AQUIFER_PROFILE TERRAIN_CLASS  SLOPE ASPECT  
+        :Units        km2               m                    deg              deg      none            none       none         none            none          none      deg      deg     
+                 1    {props[area_km2]} {props[elevation_m]} {props[lat_deg]} {props[lon_deg]} 1 FOREST FOREST DEFAULT_P [NONE] DEFAULT_T {props[slope_deg]}       0
 :EndHRUs
 
 """
-
